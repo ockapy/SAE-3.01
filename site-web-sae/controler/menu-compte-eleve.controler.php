3b92f30a8776c1133863bfa9711e544ad7968d5e
EliRoum <135995243+EliRoum@users.noreply.github.com>
2024-01-17 16:12:51 +0100

    changement du controleur du menu de compte élève

--- site-web-sae/controler/menu-compte-eleve.controler.php
+++ site-web-sae/controler/menu-compte-eleve.controler.php
@@ -6,8 +6,7 @@ require_once('../framework/view.class.php');
 //ajout des fonctions de Eleve.class.php pour récupérer l'élève connecté dans la base de données ici
 //et remplacer les valeurs des variables ci dessous 
 $login = "default";
-$nom = "default";
-$prenom = "default";
+$groupe = "default";
 $profRef = "default";
 $mdp =  "default";
 
@@ -17,8 +16,7 @@ $mdp =  "default";
 $view = new View();
 
 $view->assign('login', $login);
-$view->assign('nom', $nom);
-$view->assign('prenom', $prenom);
+$view->assign('groupe', $groupe);
 $view->assign('profRef', $profRef);
 $view->assign('mdp', $mdp);
 $view->display('menu-compte-eleve.view.php');
